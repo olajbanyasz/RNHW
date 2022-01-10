35cb47c9e058b168ca152e4d1d6ab172228a3801
André Zsolt <andrezsolt71@gmail.com>
2022-01-10 11:49:57 +0100

    main page template with search field + be connection (#1)
    * Header + input field added to layout
    
    * BE request added
    
    * details page sdded
    
    * details page swiper added
    
    * details page style fix
    
    Co-authored-by: Zsolt Andre <zsolt_andre@epam.com>

---
+++ src/containers/app/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+//import Main from '../main/Main';
+import ProductDetails from '../product-details/ProductDetails';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ProductDetails />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/style/stylesheet.tsx
@@ -0,0 +1,107 @@
+/* eslint-disable prettier/prettier */
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  headerText: {
+    color: '#FFF',
+    fontSize: 22,
+    fontWeight: '700',
+  },
+  headerStyle: {
+    color: '#FFF',
+    paddingTop: 30,
+  },
+  centerContainerStyle: {
+    position: 'absolute',
+    right: 60,
+  },
+  inputContainer: {
+    backgroundColor: '#FFF',
+    borderBottomWidth: 2,
+    borderBottomColor: 'lightgray',
+  },
+  input: {
+    borderColor: 'lightgray',
+    borderWidth: 2,
+    borderRadius: 5,
+    height: 40,
+    padding: 10,
+    margin: 20,
+  },
+  flatList: {
+    backgroundColor: '#FFF',
+    margin: 20,
+  },
+  mainContainer: {
+    backgroundColor: '#FFF',
+  },
+  item: {
+    flex: 1,
+    width: 125,
+  },
+  cardStyle: {
+    flex: 1,
+    borderTopColor: '#FFF',
+    borderRadius: 3,
+    marginBottom: 20,
+  },
+  itemPrice: {
+    fontWeight: '900',
+    marginTop: 10,
+  },
+  activityIndicator: {
+    paddingTop: 80,
+  },
+  divider: {
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  detailsContainer: {
+    backgroundColor: '#FFF',
+    marginBottom: 140,
+    padding: 20,
+    paddingBottom: 50,
+    paddingTop: 0,
+  },
+  description: {
+    paddingBottom: 30,
+  },
+  touchableOpacityStyle:{
+    position: 'absolute',
+    height: 90,
+    alignItems: 'center',
+    justifyContent: 'center',
+    bottom: 50,
+    width: '100%',
+    padding: 20,
+    paddingTop: 0,
+    backgroundColor: '#FFF',
+  },
+  floatingButtonStyle: {
+    backgroundColor: '#2196f3',
+    color: '#fff',
+    height: 50,
+    margin: 20,
+    marginTop: 0,
+    width: '100%',
+    borderRadius: 3,
+    padding: 15,
+    textAlign: 'center',
+    fontWeight: '900',
+  },
+  swiper: {
+    marginBottom: 10,
+    height: 250,
+  },
+  swiperImageView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  swiperImage: {
+    width: 150,
+    height: 150,
+  },
+});
+
+export default styles;
